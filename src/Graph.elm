948f04b553cdf09766ad18f7ee82a9c9f77071b2
lgarcin <laurent.garcin@gmail.com>
2016-11-20 18:32:30 +0100

    Style points

--- src/Graph.elm
+++ src/Graph.elm
@@ -25,15 +25,15 @@ type alias Graph =
 
 
 type alias Model =
-    Graph
+    { graph : Graph
+    , selectedRegion : Maybe Region
+    , selectedNode : Maybe Node
+    }
 
 
-twoBoundaryMove : Node -> Node -> Graph -> Graph
-twoBoundaryMove n1 n2 g =
+twoBoundaryMove : Node -> Node -> Region -> Graph -> Graph
+twoBoundaryMove n1 n2 r g =
     let
-        r =
-            Maybe.withDefault [] (List.Extra.find (\r -> List.member n1 (List.concat r) && List.member n2 (List.concat r)) g)
-
         b1 =
             Maybe.withDefault [] (List.Extra.find (List.member n1) r)
 
@@ -107,16 +107,6 @@ nbInBoundary n b =
                 )
 
 
-order : Node -> Graph -> Int
-order n g =
-    List.sum (List.concat (List.map (List.map (nbInBoundary n)) g))
-
-
-connectable : Node -> Node -> Graph -> Bool
-connectable n1 n2 g =
-    (n1 == n2 && (order n1 g) < 2) || (n1 /= n2 && (order n1 g) < 3 && (order n2 g) < 3)
-
-
 oneBoundaryMove : Node -> Node -> Region -> (Boundary -> Bool) -> Graph -> Graph
 oneBoundaryMove n1 n2 r func g =
     let
@@ -153,37 +143,92 @@ newNode g =
 
 
 type Msg
-    = Move Node Node
+    = SelectRegion Region
+    | SelectNode Node
 
 
-drawNode : Int -> Node -> Svg Msg
-drawNode ind n =
-    Svg.g [] [ circle [ cx (toString (ind * 30)), cy "0", r "5", fill "red" ] [], Svg.text' [ x (toString (ind * 30)), y "0" ] [ Svg.text (toString n) ] ]
-
-
-drawBoundary : Int -> Boundary -> Svg Msg
-drawBoundary ind b =
-    Svg.g [ transform <| "translate" ++ (toString ( 10, ind * 30 )) ] (List.indexedMap drawNode b)
-
-
-drawRegion : Region -> List (Svg Msg)
-drawRegion r =
-    (rect [ x "0", y "-15", rx "10", ry "10", width "500", height (toString ((List.length r) * 30)), fill "blue", opacity "0.1" ] []) :: (List.indexedMap drawBoundary r)
-
-
-drawGraph : Graph -> List (Svg Msg)
-drawGraph g =
+drawGraph : Model -> List (Svg Msg)
+drawGraph model =
     let
-        dr i r =
-            Svg.g [ transform <| "translate" ++ (toString ( 10, 30 + 40 * List.sum (List.map List.length (List.take i g)) )) ] (drawRegion r)
+        order node =
+            List.sum (List.concat (List.map (List.map (nbInBoundary node)) model.graph))
+
+        drawRegion ind region =
+            let
+                selectable' node =
+                    case model.selectedNode of
+                        Nothing ->
+                            (order node) < 3
+
+                        Just n ->
+                            (order node) < 2 || (n /= node && (order node) < 3)
+
+                selectable node =
+                    case model.selectedRegion of
+                        Nothing ->
+                            selectable' node
+
+                        Just r ->
+                            (selectable' node) && (region == r)
+
+                selected' node =
+                    case model.selectedNode of
+                        Nothing ->
+                            False
+
+                        Just n ->
+                            node == n
+
+                selected node =
+                    case model.selectedRegion of
+                        Nothing ->
+                            selected' node
+
+                        Just r ->
+                            (selected' node) && (region == r)
+
+                pointStyle node =
+                    (if selectable node then
+                        [ fill "orange" ]
+                     else
+                        [ fill "green" ]
+                    )
+                        ++ (if selected node then
+                                [ stroke "black" ]
+                            else
+                                []
+                           )
+
+                drawNode i node =
+                    Svg.g []
+                        [ circle ([ cx (toString (i * 30)), cy "0", r "5", onMouseDown <| SelectNode node ] ++ (pointStyle node)) []
+                        , Svg.text' [ x (toString (i * 30)), y "0" ] [ Svg.text (toString node) ]
+                        ]
+
+                drawBoundary i boundary =
+                    Svg.g [ transform <| "translate" ++ (toString ( 20, 10 + i * 40 )) ]
+                        ((rect [ x "-10", y "-15", rx "3", ry "3", width (toString ((List.length boundary) * 30)), height "30", fill "yellow" ] [])
+                            :: (List.indexedMap drawNode boundary)
+                        )
+
+                getRegionOpacity =
+                    if Just region == model.selectedRegion then
+                        "0.7"
+                    else
+                        "0.1"
+            in
+                Svg.g [ transform <| "translate" ++ (toString ( 10, 30 + 30 * ind + 40 * List.sum (List.map List.length (List.take ind model.graph)) )) ]
+                    ((rect [ x "0", y "-15", rx "10", ry "10", width "500", height (toString (10 + (List.length region) * 40)), fill "blue", opacity getRegionOpacity, onMouseOver <| SelectRegion region ] [])
+                        :: (List.indexedMap drawBoundary region)
+                    )
     in
-        List.indexedMap dr g
+        List.indexedMap drawRegion model.graph
 
 
 view : Model -> Html Msg
 view model =
     Html.div []
-        [ Svg.svg [ viewBox "0 0 600 900", Svg.Attributes.width "600px" ]
+        [ Svg.svg [ viewBox "0 0 900 900", Svg.Attributes.width "900px" ]
             (drawGraph model)
         ]
 
@@ -196,25 +241,47 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Move n1 n2 ->
-            ( twoBoundaryMove n1 n2 model, Cmd.none )
+        SelectRegion r ->
+            case model.selectedNode of
+                Nothing ->
+                    ( { model | selectedRegion = Just r }, Cmd.none )
+
+                Just _ ->
+                    ( model, Cmd.none )
+
+        SelectNode n ->
+            case model.selectedNode of
+                Nothing ->
+                    ( { model | selectedNode = Just n }, Cmd.none )
+
+                Just _ ->
+                    case ( model.selectedNode, model.selectedRegion ) of
+                        ( Just sn, Just sr ) ->
+                            ( { model | graph = twoBoundaryMove sn n sr model.graph, selectedNode = Nothing }, Cmd.none )
+
+                        ( _, _ ) ->
+                            ( model, Cmd.none )
 
 
 initialModel : Model
 initialModel =
-    [ [ [ 1, 12 ] ]
-    , [ [ 1, 12 ]
-      , [ 2, 14, 13, 3, 13, 14 ]
-      ]
-    , [ [ 4 ]
-      , [ 3, 15, 6, 16, 7, 17, 6, 15, 3, 13 ]
-      ]
-    , [ [ 5 ]
-      , [ 8, 18, 9, 19, 10, 19, 9, 21, 11, 20, 11, 21, 9, 18 ]
-      , [ 6, 17, 7, 16 ]
-      ]
-    , [ [ 11, 20 ] ]
-    ]
+    { graph =
+        [ [ [ 1, 12 ] ]
+        , [ [ 1, 12 ]
+          , [ 2, 14, 13, 3, 13, 14 ]
+          ]
+        , [ [ 4 ]
+          , [ 3, 15, 6, 16, 7, 17, 6, 15, 3, 13 ]
+          ]
+        , [ [ 5 ]
+          , [ 8, 18, 9, 19, 10, 19, 9, 21, 11, 20, 11, 21, 9, 18 ]
+          , [ 6, 17, 7, 16 ]
+          ]
+        , [ [ 11, 20 ] ]
+        ]
+    , selectedRegion = Nothing
+    , selectedNode = Nothing
+    }
 
 
 init : ( Model, Cmd Msg )
